5cb3bef7c14cede0c49fbf6634eb58aa1b954a25
CorentinDoue <corentin.doue.pro@gmail.com>
2022-12-23 16:47:03 +0100

    :wrench: migrate to vitest

--- test/cdk-integration-tests.test.ts
+++ test/cdk-integration-tests.test.ts
@@ -1,16 +1,7 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkIntegrationTests from '../lib/cdk-integration-tests-stack';
+import { describe, it } from "vitest";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-integration-tests-stack.ts
-test("SQS Queue Created", () => {
-  //   const app = new cdk.App();
-  //     // WHEN
-  //   const stack = new CdkIntegrationTests.CdkIntegrationTestsStack(app, 'MyTestStack');
-  //     // THEN
-  //   const template = Template.fromStack(stack);
-  //   template.hasResourceProperties('AWS::SQS::Queue', {
-  //     VisibilityTimeout: 300
-  //   });
+describe("cdk-integration-tests", () => {
+  it("test", () => {
+    console.log("test");
+  });
 });
