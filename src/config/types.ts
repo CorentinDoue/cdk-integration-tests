df6ea8ccb70f767d6434f8978f29b16b3d457f6a
CorentinDoue <corentin.doue.pro@gmail.com>
2022-12-31 15:21:36 +0100

    :package: commit config types

--- src/config/types.ts
+++ src/config/types.ts
@@ -1,3 +1,7 @@
 // This type is auto generated. Please don't edit it directly.
 // It will be automatically edited on cdk deploy, cdk synthesize or cdk diff.
-export type InitializedConfigs = {};
+export type InitializedConfigs = {
+  TABLE_NAME: string
+  EVENT_BUS_NAME: string
+  API_URL: string
+};
